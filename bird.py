0f4a96f4700746c167a113053010e81129f0636c
jugalthakkar <2640821+jugalthakkar@users.noreply.github.com>
2025-06-14 01:33:44 +0530

    working game

---
+++ bird.py
@@ -0,0 +1,64 @@
+import pygame
+import os
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 8
+    ANIMATION_TIME = 5
+    GRAVITY = 2.5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.velocity = 0
+        self.height = y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+        self.tick_count = 0
+
+    def jump(self):
+        self.velocity = -10.5
+        self.height = self.y
+        self.tick_count = 0
+
+    def move(self):
+        self.tick_count += 1
+        d = self.velocity * self.tick_count + 0.5 * self.GRAVITY * (self.tick_count ** 2)
+
+        if self.height + d - self.y > 40:
+            self.y += 40
+        else:
+            self.y = self.height + d
+        # if d < 0:
+        #     d -= 2
+
+        
+
+        if d < 0:
+            self.tilt = self.MAX_ROTATION
+        elif d > 0:
+            self.tilt = max(self.tilt-self.ROT_VEL,-90)
+
+
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME * 2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME * 3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME * 4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        rotated_img = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_img.get_rect(center = self.img.get_rect(topleft = (self.x,self.y)).center)
+        win.blit(rotated_img, new_rect.topleft)
---
+++ pipe.py
@@ -0,0 +1,34 @@
+import pygame
+import random
+import os
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os .path.join("imgs","pipe.png")))
+
+class Pipe:
+    GAP = 200
+    VEL = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.img_top = pygame.transform.flip(PIPE_IMG, False, True)
+        self.img_bottom = PIPE_IMG
+        self.set_height()
+        self.passed = False
+
+    def set_height(self):
+        self.height = random.randrange(50,450)
+        self.top = self.height - self.img_top.get_height()
+        self.bottom = self.height + self.GAP
+
+    def move(self):
+        self.x -= self.VEL
+        if self.x <= -self.img_top.get_width():
+            self.passed = True
+
+    def draw(self, win):
+        win.blit(self.img_top,(self.x,self.top))
+        win.blit(self.img_bottom,(self.x,self.bottom))
